1b3a887a569282347f8e9e6e37ef414c475c0f5d
kubila <dolaskyasin@gmail.com>
2020-10-15 23:56:25 +0300

    NewsCollection

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -3,28 +3,30 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NewsCollection;
 use App\Models\News;
 
 class NewsController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    return response()->json(News::all(), 200);
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //return response()->json(News::all(), 200);
+        return response(NewsCollection::collection(News::all()));
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\News  $news
-   * @return \Illuminate\Http\Response
-   */
-  public function show(News $news)
-  {
-    //
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $news)
+    {
+        //
+    }
 }
---
+++ app/Http/Resources/NewsCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'url' => $this->url,
+            'content' => $this->content,
+            'created' => $this->created,
+        ];
+    }
+}
