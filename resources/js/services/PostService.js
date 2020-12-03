ebb519b31b4dfb92a2da54dfb9dd06732a29e2cd
kubila <dolaskyasin@gmail.com>
2020-12-03 23:59:35 +0300

    PostService crud methods

--- resources/js/services/PostService.js
+++ resources/js/services/PostService.js
@@ -16,9 +16,24 @@ async function getPost(adi) {
   return await http.get('api/posts/' + adi);
 }
 
+async function createPost(params) {
+  return await http.post('api/admin/posts', params);
+}
+
+async function updatePost(post) {
+  return await http.put('api/admin/posts/' + post);
+}
+
+async function deletePost(post) {
+  return await http.delete('api/admin/posts/' + post);
+}
+
 export default {
   getPost,
   getPinnedPosts,
   getHomePosts,
-  getPosts
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost
 };
